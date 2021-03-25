710b322aaa69575c27bb8bc54d8e8f1aed239aea
Garrett <gman9mm@live.com>
2021-03-25 16:24:20 -0700

    Toggles complete on server.

--- src/app/components/todo-item/todo-item.component.ts
+++ src/app/components/todo-item/todo-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Todo } from 'src/app/models/Todo';
+import { TodoService } from '../../services/todo.service';
 
 @Component({
   selector: 'app-todo-item',
@@ -9,7 +10,7 @@ import { Todo } from 'src/app/models/Todo';
 export class TodoItemComponent implements OnInit {
   @Input() todo: Todo;
 
-  constructor() { }
+  constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
   }
@@ -26,7 +27,10 @@ export class TodoItemComponent implements OnInit {
   }
 
   onToggle(todo) {
+    // toggle in UI
     todo.completed = !todo.completed;
+    //toggle on server
+    this.todoService.toggleCompleted(todo).subscribe(todo);
   }
 
   onDelete(todo) {
